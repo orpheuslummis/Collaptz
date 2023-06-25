5ae47c5415c3ebc23ec988fedddf9f6db5bcca49
Orpheus Lummis <o@orpheuslummis.info>
2023-06-25 02:32:30 +0000

    WIP

--- examples/collatz/src/main.rs
+++ examples/collatz/src/main.rs
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{env, io::Result};
+use std::env;
 
 use collatz::do_collatz;
 use collatz_methods::COLLATZ_ID;
+use futures::executor::block_on;
 use rand::distributions::{Distribution, Uniform};
 use reqwest;
 use risc0_zkvm::serde::from_slice;
@@ -30,7 +31,7 @@ pub struct Journal {
 pub struct Output {
     pub sequence: Vec<u32>,
     pub receipt: Vec<u8>,
-    pub program_digest: String,
+    pub program_digest: [u32; 8],
 }
 
 const DEFAULT_API_URL: &'static str = "http://localhost:8000";
@@ -47,6 +48,10 @@ fn main() {
 
     let (receipt, _) = do_collatz(n);
 
+    // send your binary
+    // send your input??
+    // receive proof and output 
+
     // Just in case!
     receipt.verify(COLLATZ_ID.into()).expect(
         "Code you have proven should successfully verify; did you specify the correct image ID?",
@@ -60,10 +65,9 @@ fn main() {
     let outputs: Journal =
         from_slice(&receipt.get_journal()).expect("Journal didn't deserialize well.");
 
-    // build our output
     let out = Output {
         sequence: outputs.sequence,
-        receit: receipt.encode(),
+        receipt: receipt.encode(),
         program_digest: COLLATZ_ID,
     };
 
@@ -72,9 +76,7 @@ fn main() {
     println!("Output in JSON format: {}", out_json);
 
     let url = env::var("API_URL").unwrap_or_else(|_| DEFAULT_API_URL.to_string());
-    upload(url, out_json)
-        .await
-        .expect("Failed to upload to API");
+    upload(url, out_json).expect("Failed to upload to API");
 }
 
 // Random sample from uniform distribution: integer in [1, upper_bound].
@@ -84,16 +86,16 @@ pub fn sample_parameter(upper_bound: u64) -> u64 {
     rv.sample(&mut rng)
 }
 
-fn upload(url: String, data: String) -> Result<(), std::io::Error> {
+fn upload(url: String, data: String) -> std::result::Result<(), std::io::Error> {
     let client = reqwest::Client::new();
-    let res = client
-        .post(url)
-        .body(data)
-        .header(reqwest::header::CONTENT_TYPE, "application/json")
-        .send()
-        .await
-        .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
-
+    let res = block_on(
+        client
+            .post(url)
+            .body(data)
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .send(),
+    )
+    .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.to_string()))?;
     Ok(())
 }
 
